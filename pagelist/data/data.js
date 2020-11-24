ed8cdd38dd065c210e68ea37d0d34e30f3da8f30
ohdin <ohdin486@domfam.com>
2020-11-24 09:02:01 +0900

    Initial commit

---
+++ pagelist/data/data.js
@@ -0,0 +1,18 @@
+var data = {}
+data.common = {
+  'title': '메인',
+  'list': [
+    {
+      'stateClass': 'complete',
+      'url': 'main_design',
+      'category': '메인',
+      'page': '메인페이지 : 대쉬보드'
+    },
+    {
+      'stateClass': 'complete',
+      'url': 'sub_design',
+      'category': '메인',
+      'page': '서브페이지 : 대쉬보드'
+    }
+  ]
+};
